06240c514de6bd874f3938393857d792187944e7
Administrator <Administrator@MS-20170511IAEE>
2017-08-02 18:14:54 +0800

    fgfgffffffffff

---
+++ Web/App_Start/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.App_Start
+{
+    /// <summary>
+    /// 基础Controller
+    /// </summary>
+    public class BaseController: Controller
+    {
+        
+    }
+}
---
+++ Web/Controllers/TempletController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+
+    /// <summary>
+    /// 模板控制器
+    /// </summary>
+    public class TempletController : Controller
+    {
+        // GET: Templet
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+
+
+
+        public ActionResult List()
+        {
+            return Json(new { rows = new List<object>(), total = 0 });
+        }
+    }
+}
